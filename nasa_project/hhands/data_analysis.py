fcd7ed9b9d4ea4cddbd1f6bf86459fdc7b951c5e
Gustavo Pedroso <gpedros@its.jnj.com>
2019-10-20 01:08:19 -0300

    parsing response from NASA api and returning as GeoJson

--- nasa_project/hhands/data_analysis.py
+++ nasa_project/hhands/data_analysis.py
@@ -4,18 +4,27 @@ import requests, json
 # NASA Natural Disasters Climatic Database Raw Data
 CLIMATIC_DATABASE_URL = 'https://eonet.sci.gsfc.nasa.gov/api/v2.1/events'
 
-def getClimaticEvents():
+
+def getClimaticEvents(n):
     headers = {'Content-Type': 'application/json'}
-    response = requests.get(CLIMATIC_DATABASE_URL, headers = headers)
+    response = requests.get(CLIMATIC_DATABASE_URL + '?limit=' + str(n), headers=headers)
     response.raise_for_status()
     climatic_events = response.json()
-    print(climatic_events)
-    pass
+
+    d = {'type': 'FeatureCollection', 'features': []}
+    for i in range(n):
+        d['features'].append({'type': 'Feature',
+                              'geometry': {'type': climatic_events['events'][i]['geometries'][0]['type'],
+                                           'coordinates': climatic_events['events'][i]['geometries'][0]['coordinates']},
+                              'properties': {'event_id': climatic_events['events'][i]['id'],
+                                             'event_title': climatic_events['events'][i]['title'],
+                                             'event_date': climatic_events['events'][i]['geometries'][0]['date'],
+                                             'event_category': climatic_events['events'][i]['categories'][0]['title']
+                                             }})
+    return json.dumps(d, indent=2)
 
 
 # Test Area
 if __name__ == "__main__":
-    getClimaticEvents()
+    print(getClimaticEvents(10))
     pass
-    
-    
